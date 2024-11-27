4c367c61fb50a306a9d2d19628136b3957ee0c00
Your Name <youremail@example.com>
2024-11-27 10:25:18 +0200

    start

--- harmony/src/App.jsx
+++ harmony/src/App.jsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <ul>
+        <li id="first card" class="card">
+        <button>   
+          <p><img src="" alt="" /></p>
+          <p>title</p>
+          <p>data</p>
+          <p>author </p>
+          <p>source</p>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </li>
+
+        <li id="second card">
+        <button>   
+          <p><img src="" alt="" /></p>
+          <p>title</p>
+          <p>data</p>
+          <p>author </p>
+          <p>source</p>
+        </button>
+        </li>
+
+        <li id="third card">
+        <button>   
+          <p><img src="" alt="" /></p>
+          <p>title</p>
+          <p>data</p>
+          <p>author </p>
+          <p>source</p>
+        </button>
+        </li>
+
+        <li id="forth card">
+        <button>   
+          <p><img src="" alt="" /></p>
+          <p>title</p>
+          <p>data</p>
+          <p>author </p>
+          <p>source</p>
+        </button>
+        </li>
+
+        <li id="fhith card">
+        <button>   
+          <p><img src="" alt="" /></p>
+          <p>title</p>
+          <p>data</p>
+          <p>author </p>
+          <p>source</p>
+        </button>
+        </li>
+
+      </ul>
+    </div>
     </>
   )
 }
